cbd37c8caf6933b06d3ff6056ec669ab5fcf08f8
Aman Patel <patelpatelaman00@gmail.com>
2023-06-14 20:24:43 +0530

    lecture6  part 2

---
+++ Lecture6_2CipherSchool/Loops_part2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+
+    // int i,j,k;
+    // i=0;
+    // j=0;
+    // k=0;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // j=i++;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // k=++i;    
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // cout<<++i<<endl;
+    // for (int i = 0; i < 10; i++)
+    // {
+
+    // }
+    int i;
+    for(int i=1;i<=100;i++)
+    {
+        cout<<i<<" "; // block which gets executed everytime time
+    }
+    return 0;
+}
