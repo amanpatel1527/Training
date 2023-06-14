87dfb1248d2f3f753d4730f5264ec7a612952aea
Aman Patel <patelpatelaman00@gmail.com>
2023-06-14 20:07:05 +0530

    lecture6 part1

---
+++ Lecture6_1CipherSchool/Loops_part1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    // Looping is something in which the "similar" action is repeated
+    for(int i=1;i<=10;i++)
+    {
+        cout<<"The count is "<<i<<endl;
+    }
+    return 0;
+}
