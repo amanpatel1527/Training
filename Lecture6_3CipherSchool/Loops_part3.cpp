1f44a8499e2c0be9f90e6592b462b425e458df80
Aman Patel <patelpatelaman00@gmail.com>
2023-06-15 11:12:46 +0530

    lecture6 part 3 loops

--- Lecture6_3CipherSchool/Loops_part3.cpp
+++ Lecture6_3CipherSchool/Loops_part3.cpp
@@ -4,11 +4,23 @@
 using namespace std;
 int main()
 {
-    int i = 1;
-    while (i < 10)
+    // int i = 1;
+    // while (i < 10)
+    // {
+    //     cout << i << " "; //Loop
+    //     i++; // increment the  value
+
+    // }
+    int input;
+    int i = 100;
+    for (int i = 0; i < 101; i++)
     {
-        cout << i << " "; //Loop
-        i++; // increment the  value 
+        cin >> input;
+        if (input == 65)
+        {
+            cout << "Congrats !!!";
+            break;
+        }
     }
 
     return 0;
