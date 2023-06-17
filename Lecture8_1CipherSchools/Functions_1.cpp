fcafe4539c6228885a1ff6e3877ca2f6d1f755d6
Aman Patel <patelpatelaman00@gmail.com>
2023-06-17 11:25:26 +0530

    lecture 8 part 1

---
+++ Lecture8_1CipherSchools/Functions_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // A function is something that involves specififc task
+    // A function has two parts ==> Header And Body
+    // Every function has return value
+    
+    return 0;
+}
