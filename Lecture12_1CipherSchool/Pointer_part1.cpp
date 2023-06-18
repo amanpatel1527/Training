6f835170c4648600967ab917b37f7258e9ef9cca
Aman Patel <patelpatelaman00@gmail.com>
2023-06-18 22:56:54 +0530

    lecture 12 part1

---
+++ Lecture12_1CipherSchool/Pointer_part1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    cout<<"The address of a is "<<&a<<endl;
+    cout<<"The address of b is "<<&b<<endl;
+    return 0;
+}
