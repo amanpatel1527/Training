c2df81652fa5054542c7544e13cc902e2b56f5c1
Aman Patel <patelpatelaman00@gmail.com>
2023-06-18 15:55:33 +0530

    lecture 8 part 2

---
+++ Lecture8_2CipherSchool/function_part2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void greet() // void is used when there is no return value
+{
+    cout << "Hello World" << endl; // prints Hello World
+}
+int main()
+{
+    greet(); // prints Hello World
+    return 0;
+}
