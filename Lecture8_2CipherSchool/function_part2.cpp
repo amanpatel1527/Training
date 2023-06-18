9f2e83c33819297cabb9596c2146cbc8bb3c1cf2
Aman Patel <patelpatelaman00@gmail.com>
2023-06-18 15:58:23 +0530

    part 2 lecture 8

--- Lecture8_2CipherSchool/function_part2.cpp
+++ Lecture8_2CipherSchool/function_part2.cpp
@@ -4,8 +4,17 @@ void greet() // void is used when there is no return value
 {
     cout << "Hello World" << endl; // prints Hello World
 }
+int sum(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
 int main()
 {
+
     greet(); // prints Hello World
+    int result = sum(3, 5);
+    cout<<result;
     return 0;
 }
