afc1606a43f293e5a29ca5d7a3bb7b4e98a53023
Aman Patel <patelpatelaman00@gmail.com>
2023-06-18 20:04:33 +0530

    Lecture11

---
+++ Lecture11_1CipherSchool/AddressOfEle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+long long int a;
+int b;
+cout<<"the size of int is :"<<sizeof(a)<<" the size of long long int is "<<sizeof(b);
+return 0;
+}
