93e7e003ced8707808c9f92dd0352da418551eab
Aman Patel <patelpatelaman00@gmail.com>
2023-06-20 18:51:09 +0530

    Lecture 15 part 1

---
+++ Lecture15_1CipherSchool/string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // char r, o, h, i, t;
+    // cin >> r >> o >> h >> i >> t;
+    // cout << "The name you have just enetered is " << r << o << h << i << t << ".";
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cin >> name[i];
+    // }
+    // cout << "The name you have entered is : " << endl;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << name[i];
+    // }
+    // char name[1000];
+    // cout << "Enter your mame : (NOte : Enter # after the end of your name) ";
+    // int i = 0;
+    // while (1)
+    // {
+    //     cin >> name;
+    //     if (name[i] == '#')
+    //         break;
+    //     i++;
+    // }
+    // i = 0;
+    // cout << "The name you have entered is " << endl;
+
+    // while (name[i] != '#')
+    // {
+    //     cout << name[i];
+    // }
+    // return 0;
+    string name;
+    cin >> name;
+    cout<<name;
+    return 0;
+}
