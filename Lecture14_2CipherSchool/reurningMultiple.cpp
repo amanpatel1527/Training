0c14ccdd0829a6d21045e4373849946a72e3d8d0
Aman Patel <patelpatelaman00@gmail.com>
2023-06-20 14:32:15 +0530

    lecture14 part 2

---
+++ Lecture14_2CipherSchool/reurningMultiple.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void minandmax(int *a, int *b, int *max)
+{
+    int i;
+    int small = a[0];
+    int large = a[0];
+    for (int i = 0; i < i++;)
+    {
+        if (a[i] > large)
+        {
+            large = a[i];
+        }
+        if (a[i] < small)
+        {
+            small = a[i];
+        }
+    }
+}
+
+int main()
+{
+    int a[5];
+    int min, max;
+    for (int i = 0; i < 6; i++)
+    {
+        a[i] = i;
+    }
+    return 0;
+}
