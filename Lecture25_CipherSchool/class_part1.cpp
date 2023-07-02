fcf2bc33bbcd020a3af5c252e9b6f81ffd2cea58
Aman Patel <patelpatelaman00@gmail.com>
2023-07-02 09:52:56 +0530

    Lecture25 part1 classes part1

---
+++ Lecture25_CipherSchool/class_part1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    // There are a classof 30 students and each students has the data
+    string name;
+    int midNo;
+    float endNo;
+    bool att;
+    return 0;
+}
