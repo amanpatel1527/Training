379b85c7fca8d1ea7d023604d27375f23e8240c6
Aman Patel <patelpatelaman00@gmail.com>
2023-07-05 14:23:30 +0530

    part1 LinkedList

---
+++ Lecture28_1LinkedList/Linked_List_part1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // LinkedLisy is made up of nodes or collections of nodes
+    // nodes have 2 parts = first part containing  DATA and second part containing  address of next node
+    // something like chain 
+
+    return 0;
+}
