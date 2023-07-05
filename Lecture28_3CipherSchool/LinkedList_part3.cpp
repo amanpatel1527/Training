2e8c7b2d921098572418df59ef36f230a679fb27
Aman Patel <patelpatelaman00@gmail.com>
2023-07-05 20:58:42 +0530

    .

--- Lecture28_3CipherSchool/LinkedList_part3.cpp
+++ Lecture28_3CipherSchool/LinkedList_part3.cpp
@@ -31,27 +31,36 @@ void printLinkedList(node *&head)
     while (temp != NULL)
     {
         cout << temp->data << "-->";
+        temp = temp->next;
     }
 };
 int main()
 {
 
     // Head of linked list iis nothing but a fancy term to indicate the top or the first node
-    node *head;
+    node *head =NULL;
 
     printLinkedList(head);
 
     node *node1 = new node(3);
     head = node1;
     printLinkedList(head);
+    cout << endl;
 
     node *node2 = new node(5);
     node1->next = node2;
     printLinkedList(head);
+    cout << endl;
 
     node *node3 = new node(7);
     node2->next = node3;
     printLinkedList(head);
+    cout << endl;
+
+    node *node4 = new node(9);
+    node3->next = node4;
+    printLinkedList(head);
+    cout<<endl;
 
     // printnode(node1);
     // printnode(node2);
@@ -63,8 +72,8 @@ int main()
     // cout << endl;
     // printnode(node3);
 
-    printnode(node1->next);       // node2 = node1->next;
-    printnode(node1->next->next); // node3 = node2->next = node1->next->next
+    // printnode(node1->next);       // node2 = node1->next;
+    // printnode(node1->next->next); // node3 = node2->next = node1->next->next
 
     return 0;
 }
