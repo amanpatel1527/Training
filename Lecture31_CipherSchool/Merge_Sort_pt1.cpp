089a529a276df197892113ca1cf87f6e3066239c
Aman Patel <patelpatelaman00@gmail.com>
2023-07-09 10:27:40 +0530

    Lecture31 pt1

---
+++ Lecture31_CipherSchool/Merge_Sort_pt1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Divide And Conquer Alogo
+// Focus on how to solve the  problem
+// divide the problem into smalls parts and then conquer them
+
+int main()
+{
+    
+    return 0;
+}
